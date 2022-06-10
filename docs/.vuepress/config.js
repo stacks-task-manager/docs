2cb62bb76a9e8e59a8705e349882b2544b6fd7c5
Cristian Barlutiu <cristian.barlutiu@gmail.com>
2022-06-10 20:09:07 +0200

    updates

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,104 +11,100 @@ module.exports = {
       { text: 'Site', link: 'https://stacks.rocks', target: '_self', rel: '' },
     ],
     sidebar: [
-        {
-            title: 'General',
-            collapsable: false,
-            children: [
-                '/general/layout.md',
-                '/general/sidebar.md',
-                '/general/documents.md',
-                '/general/changing-the-language.md',
-                '/general/add-language.md',
-                '/general/create-new-document.md',
-                '/general/rename-document.md',
-                '/general/change-theme.md',
-                '/general/open-preferences.md',
-            ]
-        },
-        {
-            title: 'Workspaces',
-            collapsable: false,
-            children: [
-                '/workspaces/what-is-a-workspace.md',
-                '/workspaces/moving-a-workspace.md',
-                '/workspaces/add-new-workspace.md',
-                '/workspaces/renaming-a-workspace.md',
-                '/workspaces/removing-a-workspace.md',
-                '/workspaces/changing-workspace-icon.md',
-            ]
-        },
-        {
-            title: 'Projects',
-            collapsable: false,
-            children: [
-                '/projects/board-view.md',
-                '/projects/list-view.md',
-                '/projects/table-view.md',
-                '/projects/calendar-view.md',
-                '/projects/stack.md',
-                '/projects/task.md',
-                '/projects/task-details.md',
-                '/projects/tags.md',
-                '/projects/statuses.md',
-                '/projects/settings.md',
-            ]
-        },
-        {
-            title: 'People',
-            collapsable: false,
-            children: []
-        },
-        {
-            title: 'Notepad',
-            collapsable: false,
-            children: [
-                '/notepad/add-image.md',
-                '/notepad/resize-image.md',
-            ]
-        },
-        {
-            title: 'Files',
-            collapsable: false,
-            children: []
-        },
-        {
-            title: 'License',
-            collapsable: false,
-            children: [
-                '/license/use-license.md',
-                '/license/buy-license.md',
-                '/license/manage-linked-devices.md',
-            ]
-        },
-        {
-            title: 'Syncing',
-            collapsable: false,
-            children: [
-                '/syncing/sync-with-google-drive.md',
-                '/syncing/sync-with-dropbox.md',
-            ]
-        },
-        {
-            title: 'Backup',
-            collapsable: false,
-            children: [
-                '/backup/manual-backup.md',
-                '/backup/automatic-backup.md',
-                '/backup/restore-backup.md',
-            ]
-        },
-        {
-            title: 'Other',
-            collapsable: false,
-            children: [
-                '/other/joining-beta-program.md',
-            ]
-        },
-        {
-            title: 'Markdown',
-            path: '/markdown/',
-        },
+      {
+        title: 'General',
+        collapsable: false,
+        children: [
+          '/general/layout.md',
+          '/general/sidebar.md',
+          '/general/documents.md',
+          '/general/changing-the-language.md',
+          '/general/add-language.md',
+          '/general/create-new-document.md',
+          '/general/rename-document.md',
+          '/general/change-theme.md',
+          '/general/open-preferences.md',
         ],
+      },
+      {
+        title: 'Workspaces',
+        collapsable: false,
+        children: [
+          '/workspaces/what-is-a-workspace.md',
+          '/workspaces/moving-a-workspace.md',
+          '/workspaces/add-new-workspace.md',
+          '/workspaces/renaming-a-workspace.md',
+          '/workspaces/removing-a-workspace.md',
+          '/workspaces/changing-workspace-icon.md',
+        ],
+      },
+      {
+        title: 'Projects',
+        collapsable: false,
+        path: '/projects/',
+        children: [
+          '/projects/board-view.md',
+          '/projects/list-view.md',
+          '/projects/table-view.md',
+          '/projects/calendar-view.md',
+          '/projects/stack.md',
+          '/projects/task.md',
+          '/projects/task-details.md',
+          '/projects/tags.md',
+          '/projects/statuses.md',
+          '/projects/settings.md',
+        ],
+      },
+      {
+        title: 'People',
+        collapsable: false,
+        children: [],
+      },
+      {
+        title: 'Notepad',
+        collapsable: false,
+        children: ['/notepad/add-image.md', '/notepad/resize-image.md'],
+      },
+      {
+        title: 'Files',
+        collapsable: false,
+        children: [],
+      },
+      {
+        title: 'License',
+        collapsable: false,
+        children: [
+          '/license/use-license.md',
+          '/license/buy-license.md',
+          '/license/manage-linked-devices.md',
+        ],
+      },
+      {
+        title: 'Syncing',
+        collapsable: false,
+        children: [
+          '/syncing/sync-with-google-drive.md',
+          '/syncing/sync-with-dropbox.md',
+        ],
+      },
+      {
+        title: 'Backup',
+        collapsable: false,
+        children: [
+          '/backup/manual-backup.md',
+          '/backup/automatic-backup.md',
+          '/backup/restore-backup.md',
+        ],
+      },
+      {
+        title: 'Other',
+        collapsable: false,
+        children: ['/other/joining-beta-program.md'],
+      },
+      {
+        title: 'Markdown',
+        path: '/markdown/',
+      },
+    ],
   },
 };
--- docs/general/change-theme.md
+++ docs/general/change-theme.md
@@ -2,3 +2,8 @@
 
 1. Open the [**Preferences**](./open-preferences.md)
 2. In the **Preferences**, click on the **Themes** tab, then choose theme you want between light and dark.
+
+<video autoplay="autoplay" class="img-fluid mx-auto radius-big shadow show-on-dark" loop="loop" muted="muted" playsinline="playsinline" width="100%">
+    <source :src="$withBase('/assets/img/general/change-theme.mp4')">
+    <source :src="$withBase('/assets/img/general/change-theme.webm')">
+</video>
